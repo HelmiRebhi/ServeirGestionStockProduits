c872d9b53db1318f7d56d2674b477fb13caea855
HelmiRebhi <rebhi7helmi@gmail.com>
2018-11-12 21:53:05 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeirGestionStockProduitsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeirGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeirGestionStockProduitsApplication.class, args);
+	}
+}
